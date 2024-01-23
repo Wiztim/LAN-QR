c44cf186159b94e130000ce4865e8f51d3e5f2cf
wiztim <timothyorlov@gmail.com>
2024-01-22 19:09:41 -0800

    added qr code display

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 8080
+var qr = require('qr-image')
+
 //const localIP = '0.0.0.0'
 //const localIP = '172.21.223.210'
 
-app.use(express.static('frontend'));
+app.use(express.static('frontend'))
 
-app.post('/api/data', (req, res) => {
-    const requestData = req.body;
-    // Do something with the posted data (e.g., save it to a database)
-    console.log('Received POST request with data:', requestData);
-    res.json({ message: 'Data received successfully' });
-  });
+// handle the upload request
+app.post('/upload', (req, res) => {
+  // might need this
+  const requestData = req.body;
+  console.log('Received POST request with data:', req.get('origin'))
+  
+  // create the qr png
+  var qrpng = qr.image('/frontend/uploads/', {type:'png'})
+  qrpng.pipe(require('fs').createWriteStream('./frontend/uploads/test.png'))
+
+  // TODO: fix qr png url
+  var qrpngURL = '/uploads/test.png'
+  res.setHeader('Content-Type', 'application/json')
+  res.json({'qrCodeUrl': qrpngURL}).status(200)
+});
+
+// handle sending the URL and file download
+app.get('/frontend/uploads/*', (req, res) => {
+  res.setHeader('content-type', 'image/png');
+  console.log('in get requst: ', req.path)
+  var imgPath = '/frontend/uploads/test.png'
+  res.send(imgPath)
+})
 
 app.listen(port, () => {
-  console.log(`Server is running at ${port}`);
+  console.log(`Server is running at ${port}`)
 });
